0813441fbb37ef7d16b62131e3414a9ff04787f1
Angello 5 <angello.llerena@usil.pe>
2024-05-26 23:31:38 -0500

    segundo avance

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Antlr4.Runtime;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var input = new AntlrInputStream("tu codigo aqui");
+        var lexer = new GamecsharpLexer(input);
+        var tokens = new CommonTokenStream(lexer);
+        var parser = new GamecsharpParser(tokens);
+
+        var context = parser.prog();  // 'prog' es la regla inicial de tu gramática
+    }
+}
